f3475eb2201ef9e40b6f583ca02befda707427a8
Anna Sudol <anjasudol@gmail.com>
2022-11-23 11:16:09 +0000

    vals

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Hash } from 'crypto';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('last-block')
+  getLastBlock() {
+    return this.appService.getLastBlock('latest');
   }
+  @Get('block/:hash')
+  getBlock(@Param('hash') hash: string) {
+    return this.appService.getLastBlock(hash);
+  }
+
+  // @get('address')
+  // getContractAddress()
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { ethers } from 'ethers';
+import * as tokenJson from './abi.json';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getLastBlock(blockHashOrBlockTag: string): Promise<ethers.providers.Block> {
+    return ethers.getDefaultProvider('goerli').getBlock(blockHashOrBlockTag);
   }
+  // provider: ethers.providers.BaseProvider;
+
+  // getBlock(blockHashOrBlockTag: string): Promise<ethers.providers.Block> {
+  //   return ethers.getDefaultProvider('goerli').getBlock(blockHashOrBlockTag);
+  // }
+
+  // getContractAddress(address: string) {
+  //   const contact = new ethers.Contract(address, tokenJson.abi, this.provider);
+  //   return contact.;
+  // }
 }
